22802fb0c2518ea4ae3b2375e9b1c1f45ef0e316
jhon_chen <jhon.chen@gmail.com>
2014-11-20 10:42:33 +0800

    添加事物

--- app/controllers/DealrecordController.php
+++ app/controllers/DealrecordController.php
@@ -100,8 +100,11 @@ class DealrecordController extends Controller{
 	}
 
 	public function delete($customer_id,$id){
-		Commission::where('dealrecord_id', $id)->delete();
-		Dealrecord::destroy($id);
+		DB::transaction(function() use ($id){
+			Commission::where('dealrecord_id', $id)->delete();
+			Dealrecord::destroy($id);
+		});
+
 		return Redirect::action('DealrecordController@toList', array('customer_id'=>$customer_id));
 	}
 }
